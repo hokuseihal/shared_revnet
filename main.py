b473ee9980ab92367062c2e2c8a75cb3b0044310
hokuseihal <hokuseihal@gmail.com>
2022-02-09 18:03:18 +0900

    fix transformer

--- main.py
+++ main.py
@@ -76,12 +76,6 @@ if __name__ == "__main__":
     device = args.device if args.device == 'cuda' and torch.cuda.is_available() else 'cpu'
     criterion = nn.CrossEntropyLoss()
     train_transform = T.Compose([
-        T.ToTensor(),
-        T.Normalize(
-            (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
-            (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
-    ])
-    val_transform = T.Compose([
         T.RandomCrop(32, padding=4),
         T.RandomHorizontalFlip(),
         T.RandomRotation(15),
@@ -90,6 +84,13 @@ if __name__ == "__main__":
             (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
             (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
     ])
+    val_transform = T.Compose([
+        T.ToTensor(),
+        T.Normalize(
+            (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
+            (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
+    ])
+
     trainloader = torch.utils.data.DataLoader(
         Dataset(train=True, download=True, transform=train_transform, root='../data/cifar100'),
         batch_size=args.batchsize,
