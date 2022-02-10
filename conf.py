fa4c1b1fb79ee726886737033c76ebe0906dd55c
hokuseihal <hokuseihal@gmail.com>
2022-02-10 20:32:25 +0900

    add own setting

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 import torch
-from timm.scheduler.step_lr import StepLRScheduler
+import torchvision.transforms as T
 
 
 def get_optim_scheduler(key, model, epoch=None):
@@ -7,13 +7,65 @@ def get_optim_scheduler(key, model, epoch=None):
         optimizer = torch.optim.SGD(lr=0.1, momentum=0.9, weight_decay=1e-4, params=model.parameters())
         scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[epoch // 3, 2 * epoch // 3],
                                                          gamma=0.1)
-        return optimizer, scheduler
+        train_transform = T.Compose([
+            T.RandomHorizontalFlip(),
+            T.ToTensor(),
+            T.Normalize(
+                (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
+                (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
+        ])
+        val_transform = T.Compose([
+            T.ToTensor(),
+            T.Normalize(
+                (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
+                (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
+        ])
+        return optimizer, scheduler, train_transform, val_transform
     if (key == 'resnetcifar100'):
-
         optimizer = torch.optim.SGD(lr=0.1, momentum=0.9, weight_decay=5e-4, params=model.parameters())
-        # scheduler = StepLRScheduler(optimizer, decay_t=epoch // 4, decay_rate=2e-1, warmup_t=400, warmup_lr_init=0)
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60*400,120*400,160*400],gamma=0.2)  # learning rate decay
-        return optimizer, scheduler
+        scheduler = StepLRScheduler(optimizer, decay_t=epoch // 4, decay_rate=2e-1, warmup_t=400, warmup_lr_init=0)
+        # scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60*400,120*400,160*400],gamma=0.2)  # learning rate decay
+        train_transform = T.Compose([
+            T.RandomCrop(32, padding=4),
+            T.RandomHorizontalFlip(),
+            T.RandomRotation(15),
+            T.ToTensor(),
+            T.Normalize(
+                (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
+                (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
+        ])
+        val_transform = T.Compose([
+            T.ToTensor(),
+            T.Normalize(
+                (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
+                (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
+        ])
+        return optimizer, scheduler, train_transform, val_transform
+    if (key == 'own'):
+        optimizer = torch.optim.SGD(lr=0.1, momentum=0.9, weight_decay=1e-3, params=model.parameters())
+        scheduler = StepLRScheduler(optimizer, decay_t=epoch // 4, decay_rate=2e-1, warmup_t=400, warmup_lr_init=0)
+        train_transform = T.Compose([
+            T.RandomCrop(32, padding=4),
+            T.RandomHorizontalFlip(),
+            T.RandomRotation(15),
+            T.ColorJitter(),
+            T.ToTensor(),
+            T.RandomErasing(),
+            T.RandomAutocontrast(),
+            T.RandomGrayscale(),
+            T.RandomVerticalFlip(),
+            T.ToTensor(),
+            T.Normalize(
+                (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
+                (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
+        ])
+        val_transform = T.Compose([
+            T.ToTensor(),
+            T.Normalize(
+                (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
+                (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
+        ])
+        return optimizer, scheduler, train_transform, val_transform
     else:
         raise NotImplementedError
 
--- main.py
+++ main.py
@@ -3,17 +3,12 @@ from multiprocessing import cpu_count
 import numpy as np
 import torch
 import torch.nn as nn
-import torchvision.transforms as T
 from torchvision.datasets import CIFAR100 as Dataset
 
 from conf import get_optim_scheduler
 from model import modeldic
 
-seed=0
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.backends.cudnn.benchmark = False
-torch.backends.cudnn.deterministic = True
+
 def operate(phase):
     mloss = []
     macc = []
@@ -40,12 +35,9 @@ def operate(phase):
                 loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
-                if e == 0:
-                    warmup_scheduler.step()
-                if (scheduler is not None and e>=1):
+                if (scheduler is not None):
                     scheduler.step(epoch=e * len(trainloader) + idx)
                     log.update({'lr': optimizer.param_groups[0]['lr']})
-
 
                 if (args.wandb): wandb.log(log)
             print(f"{e=},{idx}/{len(loader)},{loss=:2.4f},{acc=:2.4f},lr={optimizer.param_groups[0]['lr']:.3e},{phase}")
@@ -66,31 +58,17 @@ if __name__ == "__main__":
     parser.add_argument('--model', default='revnet18_r2')
     parser.add_argument('--paramshare', default=1, type=int)
     parser.add_argument('--wandb', default=False, action='store_true')
-    parser.add_argument('--lr', default=0.0001, type=float)
-    parser.add_argument('--optim', default='adam')
     parser.add_argument('--tag', default='')
-    parser.add_argument('--setting', default=None)
+    parser.add_argument('--setting', required=True)
     args = parser.parse_args()
     if (args.wandb): import wandb
 
     device = args.device if args.device == 'cuda' and torch.cuda.is_available() else 'cpu'
     criterion = nn.CrossEntropyLoss()
-    train_transform = T.Compose([
-        T.RandomCrop(32, padding=4),
-        T.RandomHorizontalFlip(),
-        T.RandomRotation(15),
-        T.ToTensor(),
-        T.Normalize(
-            (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
-            (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
-    ])
-    val_transform = T.Compose([
-        T.ToTensor(),
-        T.Normalize(
-            (0.5070751592371323, 0.48654887331495095, 0.4409178433670343),
-            (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)),
-    ])
 
+    model = modeldic[args.model](num_classes=100, parameter_share=args.paramshare).to(device)
+    optimizer, scheduler, train_transform, val_transform = get_optim_scheduler(args.setting, model,
+                                                                               args.epoch * 50000 // args.batchsize)
     trainloader = torch.utils.data.DataLoader(
         Dataset(train=True, download=True, transform=train_transform, root='../data/cifar100'),
         batch_size=args.batchsize,
@@ -101,21 +79,11 @@ if __name__ == "__main__":
         batch_size=args.batchsize,
         shuffle=True,
         num_workers=cpu_count())
-    model = modeldic[args.model](num_classes=100, parameter_share=args.paramshare).to(device)
-    if (args.optim == 'adam'):
-        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    elif (args.optim == 'sgd'):
-        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
-    else:
-        assert NotImplementedError
 
-    scheduler = None
-    from cifartrain import WarmUpLR
-    if (args.setting is not None):
-        optimizer, scheduler = get_optim_scheduler(args.setting, model, args.epoch * len(trainloader))
-    warmup_scheduler = WarmUpLR(optimizer, len(trainloader))
-
-    if (args.wandb): wandb.init(project='shared_revnet', name=args.model + args.tag)
+    if (args.wandb): wandb.init(project='shared_revnet', name=args.model + args.tag,
+                                config={"optimizer": str(optimizer), "scheduler": str(scheduler), "model": args.model,
+                                        "batchsize": args.batchsize, "train_transform": str(train_transform),
+                                        'val_transform': str(val_transform), "setting": args.setting})
     for e in range(args.epoch):
         operate('train')
         operate('val')
